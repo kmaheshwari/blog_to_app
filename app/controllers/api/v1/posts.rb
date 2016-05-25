71eae176ccd95d2ea24885817223e1fa803ed2ec
kmaheshwari <a.thakur73.at@gmail.com>
2016-05-25 15:18:26 +0530

    minor issue

--- app/controllers/api/v1/posts.rb
+++ app/controllers/api/v1/posts.rb
@@ -2,12 +2,7 @@ module API
   module V1
     class Posts < Grape::API
       include API::V1::Defaults
-<<<<<<< HEAD
-      
-=======
       helpers PostHelper
-
->>>>>>> development
       resource :posts do
         desc "Return all posts"
         get "", root: :posts do

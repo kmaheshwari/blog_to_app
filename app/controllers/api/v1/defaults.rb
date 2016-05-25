efdf9a05af04ee8802249b220baf31ca2bb6b511
kmaheshwari <a.thakur73.at@gmail.com>
2016-05-25 14:55:32 +0530

    api authentication added

--- app/controllers/api/v1/defaults.rb
+++ app/controllers/api/v1/defaults.rb
@@ -2,8 +2,9 @@ module API
   module V1
     module Defaults
       extend ActiveSupport::Concern
-
+      # include ActionController::HttpAuthentication::Token::ControllerMethods
       included do
+        ActionController::HttpAuthentication::Token
         prefix "api"
         version "v1", using: :path
         default_format :json
@@ -18,8 +19,14 @@ module API
             Rails.logger
           end
         end
+        #api authentication
+        before do
+          access=App.where(access_token: headers['Authorization'])
+          error!('Unauthorized', 401) unless not access.empty?
+        end
 
       end
     end
+
   end
 end
--- app/controllers/api/v1/posts.rb
+++ app/controllers/api/v1/posts.rb
@@ -2,7 +2,7 @@ module API
   module V1
     class Posts < Grape::API
       include API::V1::Defaults
-
+      
       resource :posts do
         desc "Return all posts"
         get "", root: :posts do

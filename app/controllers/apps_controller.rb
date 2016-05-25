4ea80ea1318bcae16a681f16ef945aff2cdb86ce
kmaheshwari <a.thakur73.at@gmail.com>
2016-05-26 00:14:01 +0530

    cache generalised

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -1,7 +1,6 @@
 class AppsController < ApplicationController
   before_action :set_app, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_author!
-  before_action :generate_access_token, only: [:create]
 
   # GET /apps
   # GET /apps.json
@@ -101,13 +100,11 @@ class AppsController < ApplicationController
       params.require(:app).permit(:app_icon, :app_name,:app_url,:author_id,:contact_email)
     end
 
-<<<<<<< HEAD
+
     def generate_access_token
     begin
       self.access_token = SecureRandom.hex
     end while self.class.exists?(access_token: access_token)
   end
-=======
 
->>>>>>> development
 end

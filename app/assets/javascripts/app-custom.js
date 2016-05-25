ad913f1a5ff9f756ea221b60078951b3a2e8608d
rajiv-shrivastava <rjv834@gmail.com>
2016-05-25 15:31:03 +0530

    catgorgies on phone added

--- app/assets/javascripts/app-custom.js
+++ app/assets/javascripts/app-custom.js
@@ -11,8 +11,39 @@
         function changecolor(value)
         {
              var color_value = $("#color-element").val()
-             $("#header-app").css("background-color", "#"+color_value)
+             $("#header-app").css("background-color", "#"+color_value);
+             $("#all-cateogries").css("background-color", "#"+color_value)
+             $("#catog-div").css("background-color", "#"+color_value)
+             
         }     
 
 
 
+       function catog()
+
+       {
+               var catog = " "
+               catog = $(".dropdown-toggle").attr("title");
+
+
+                   var chop_catog = catog.split(",");
+
+               if (chop_catog[1] == undefined)
+               {
+               $("#all-cateogries").html(chop_catog[0] + ' ');
+               
+               }
+
+               else if (chop_catog[2] != undefined )
+               {
+                  $("#all-cateogries").html(chop_catog[0] + chop_catog[1] + chop_catog[2] );
+
+               }  
+
+               else if (chop_catog[1] != undefined && chop_catog[2] == undefined )
+               {
+                  $("#all-cateogries").html(chop_catog[0] +  chop_catog[1]);
+
+               }  
+
+       }

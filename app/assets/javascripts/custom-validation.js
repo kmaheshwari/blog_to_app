dbd2575f60b334569a347d44e4912206aeddabdd
rajiv-shrivastava <rjv834@gmail.com>
2016-05-28 18:29:49 +0530

    email validation working in register

--- app/assets/javascripts/custom-validation.js
+++ app/assets/javascripts/custom-validation.js
@@ -31,7 +31,7 @@ $(document).ready(function() {
                     categories: 
                     {
                         required: true,
-                        // email: true
+                        
                     },
                 },
             });

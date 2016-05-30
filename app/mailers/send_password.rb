80240c712cf816c2b21df97fedab28de9e853be0
rajiv-shrivastava <rjv834@gmail.com>
2016-05-30 14:05:56 +0530

    arranged app model

--- app/mailers/send_password.rb
+++ app/mailers/send_password.rb
@@ -3,10 +3,10 @@ class SendPassword < ApplicationMailer
 	default :from => 'blappr@risemetric.com'
 
   # send a signup email to the user, pass in the user object that   contains the user's email address
-  def send_signup_email(user,temp_pass)
-    @user = user
+  def send_signup_email(author_email,temp_pass)
+    @author_email = author_email
     @temp_pass = temp_pass
-    mail( :to => @user,
+    mail( :to => @author_email,
     :subject => 'Thanks for signing up for our amazing app' )
   end
 

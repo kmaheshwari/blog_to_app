ba5166987941a2bad61e4d70cdc6926bb5e30d9a
Dhaval Purohit <dhaval.purohit08@gmail.com>
2016-05-30 13:06:34 +0530

    adds analytics, url

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,8 @@ Rails.application.routes.draw do
   get '/faq' => 'apps#faq'
   get '/monetize' => 'apps#monetize'
   get '/posts' => 'apps#posts'
-  get 'new_notification' => 'apps#push_notification'
-  get 'all_notification' => 'apps#all_notification'
+  get '/new_notification' => 'apps#push_notification'
+  get '/all_notification' => 'apps#all_notification'
   get 'support' => 'apps#support'
   get 'registration' => 'static#registration'
 

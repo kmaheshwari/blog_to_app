9628f652b42f270ca6d4d6b452b030b78bafe93d
rajiv-shrivastava <rjv834@gmail.com>
2016-06-01 04:10:45 +0530

    validations reimproved

--- app/assets/javascripts/custom-validation.js
+++ app/assets/javascripts/custom-validation.js
@@ -1,60 +1,49 @@
-  $(document).ready(function() {
 
-
+$(document).ready(function() {
 
   $("#basicform").validate({
                 rules: {
                     blog_url: { required: true,url: true },
                     
                     email: {required: true,email: true}
-                    }
-            });
+                    ,}
+            })
 
 
   $("#app-form").validate({
                 rules: {
-                    app_name: { required: true },
-                    categories: {required: true }
+                    app_name: { required: true, },
+
+                    cateogories: { required: true, }
+                    
+                },
 
                 messages: {
-                           app_name: {required: "Name of app is missing",},
+                           app_name: {
+                            required: "Enter Your App name",
+                           },
                           }
-            }});
+                });
 
-
-  $("#reset-form").validate({
-                
-      rules: 
-      {
-        "author[current_password]": { required: true},
-
-       "author[password]": { required: true ,minlength : 8},
+  $("#loginform").validate({
+              rules: {
+                   "author[email]": { required: true},
                     
-       "author[password_confirm]": {required: true,equalTo :"#author[password]"}
-                    
-        }  ,
-        
-        messages: 
-    {
-        "author[password]": {
-            required: "Please provide a password",
-            minlength: "Your password must be at least 8 characters long"
-        },
-        "author[password_confirm]": {
-            equalTo: "Please enter the same password as above"
-        }
-        
-    }
+                    "author[password]": {required: true}
+                    },
+
+                messages: {
+                           "author[email]": {required: "Username can't be empty"},
+
+                           "author[password]": {required: "Password can't be empty"}
+
+                          }
+            });
+
+
+
 
 });
-
-
-
-
-
-
-
- });
 
 
 

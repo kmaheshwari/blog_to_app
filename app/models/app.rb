473ef95de79d4e7045bc439cded5884ac54e9cc7
kmaheshwari <maheshwari.kajol@gmail.com>
2016-05-31 23:44:42 +0530

    adds nested forms

--- app/models/app.rb
+++ app/models/app.rb
@@ -1,9 +1,6 @@
 class App < ActiveRecord::Base
 	belongs_to :author
-	mount_uploader :app_icon, IconUploader
-
-     
-
+	mount_uploader :app_icon, IconUploader  
     has_many :appcategories
     has_many :appcolours
     has_many :app_pages

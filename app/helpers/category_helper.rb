dd0644d976835170e6d63e4c32c13d45b5d60b70
Dhaval Purohit <dhaval.purohit08@gmail.com>
2016-05-31 23:20:55 +0530

    populates category field

--- app/helpers/category_helper.rb
+++ app/helpers/category_helper.rb
@@ -7,4 +7,16 @@ module CategoryHelper
 	  categories.each {|category| category.slice!(*@@attributes)}
 	  categories
 	end	
+
+	def populate base_uri
+		categories=HTTParty.get(base_uri+"/?json=get_category_index")
+		categories=categories["categories"]
+		categories.each {|category| category.slice!("title")}
+
+		pages=HTTParty.get(base_uri+"/?json=get_page_index")
+		pages=pages["pages"]
+		pages.each {|page| page.slice!("title")}
+		
+		{"categories" => categories, "pages"=>pages}
+	end	
 end

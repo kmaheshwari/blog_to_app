b0b285a9f343fb480b695f070d0e91f12702ee4a
rajiv-shrivastava <rjv834@gmail.com>
2016-05-31 17:13:23 +0530

    alerts working

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -34,7 +34,7 @@ class AppsController < ApplicationController
   def update
     @app = App.find_by(:author_id =>current_author.id)
     
-   
+    binding.pry
     @app.app_name = params[:app_name]
     @app.contact_email = params[:email]
 
--- app/models/app.rb
+++ app/models/app.rb
@@ -2,13 +2,7 @@ class App < ActiveRecord::Base
 	belongs_to :author
 
      
-    validates_presence_of :app_name, :on => :update
-	validates_presence_of :app_url 
 
-
-	validates :app_name, uniqueness: true
-    validates :app_url, uniqueness: true
-    
     has_many :appcategories
     has_many :appcolours
     has_many :app_pages

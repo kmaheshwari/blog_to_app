2eef4967de2d8b7c733c0c7824a5bac3326aafe6
kmaheshwari <a.thakur73.at@gmail.com>
2016-06-03 17:38:18 +0530

    set app modeified

--- app/controllers/apps_controller.rb
+++ app/controllers/apps_controller.rb
@@ -165,7 +165,11 @@ class AppsController < ApplicationController
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_app
-    @app = App.find_by(:author_id =>$current_author.id)
+      if current_author
+        @app = App.find_by(:author_id =>current_author.id)
+      else
+        @app = App.find_by(:author_id =>$current_author.id)
+      end
     end
 
     # Never trust parameters from the scary internet, only allow the white list through.

c82cf4251b757dcdf51e1067551773cd9080f45e
kmaheshwari <a.thakur73.at@gmail.com>
2016-06-03 17:33:21 +0530

    order status entry done

--- app/controllers/payments_controller.rb
+++ app/controllers/payments_controller.rb
@@ -18,12 +18,15 @@ class PaymentsController < ApplicationController
     )
 
   # byebug
-  @app_id=App.find_by(author_id: $current_author.id).id
+  @app=App.find_by(author_id: $current_author.id)
+  @app_id=@app.id
   @pay=Payment.new(customer_id: @customer.id,amount:params[:amount],app_id: @app_id,status: @charge["status"])
 
         if  @pay.save
           sign_in $current_author
           $current_author.update(author_active: true)
+          @app_order = OrderState.new()
+          @app_order.update(app_version_name: @app.app_name,app_status: "pending",generated_date: Date.today,author_id: current_author.id )
           PaymentMail.perform_async(current_author.email)
         end
         
